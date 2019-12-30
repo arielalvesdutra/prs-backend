de69f6ec73211098bc0ae9ac14633d7412a2e72e
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-12-30 08:11:52 -0300

    Commit inicial

---
+++ src/main/java/dev/arielalvesdutra/prs/PrsApplication.java
@@ -0,0 +1,13 @@
+package dev.arielalvesdutra.prs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrsApplication.class, args);
+	}
+
+}
